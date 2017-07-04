9031bdaf13815a295e044d26d610bad26f04475f
Salman <salkhan23@gmail.com>
2017-07-03 20:10:49 -0400

    Add a script to plot the layer activations and convolutional filters of a layer

---
+++ prebuilt_learned_lateral_weights.py
@@ -0,0 +1,65 @@
+# -------------------------------------------------------------------------------------------------
+# Use the model learned from learned_lateral_weights.py for further exploration
+#
+# Author: Salman Khan
+# Date  : 29/06/17
+# -------------------------------------------------------------------------------------------------
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+
+from keras.models import load_model
+
+
+import learned_lateral_weights
+import utils
+reload(utils)  # Force Reload utils to pick up latest changes
+reload(learned_lateral_weights)
+
+
+if __name__ == "__main__":
+
+    plt.ion()
+
+    # 1. Get MNIST Data
+    # --------------------------------------------
+    x_train, y_train, x_test, y_test, x_sample, y_sample = utils.get_mnist_data()
+
+    # 2. Load the Model
+    # -------------------------------------------
+    contour_integration_model = load_model(
+        "learned_lateral_weights_3x3_overlap.hf",
+        custom_objects={'ContourIntegrationLayer': learned_lateral_weights.ContourIntegrationLayer()}
+    )
+    # contour_integration_model.summary()
+
+    # 3. Show activation volumes before and after the lateral connections
+    # -----------------------------------------------------------------------------------
+    utils.display_layer_activations(contour_integration_model, 0, x_sample)
+    utils.display_layer_activations(contour_integration_model, 1, x_sample)
+
+    # 4. Show Feed-forward and lateral Filters
+    # --------------------------------------------------------------------
+    weights_ff = contour_integration_model.layers[0].get_weights()[0]
+    weights_l = contour_integration_model.layers[1].get_weights()[0]
+
+    # Normalize the display
+    max_filt_value = np.max([np.max(weights_ff), np.max(weights_l)])
+    min_filt_value = np.min([np.min(weights_ff), np.min(weights_l)])
+
+    fig_dim = np.int(np.round(np.sqrt(weights_ff.shape[-1])))
+
+    # Plot the feed forward Filters
+    fig = plt.figure()
+    fig.suptitle("FF convolution Filters")
+    for ch_idx in range(weights_ff.shape[-1]):
+        fig.add_subplot(fig_dim, fig_dim, ch_idx + 1)
+        plt.imshow(weights_ff[:, :, 0, ch_idx], cmap='Greys', vmin=min_filt_value, vmax=max_filt_value)
+
+    # Plot the lateral connections filters
+    fig = plt.figure()
+    fig.suptitle("Lateral Connections Filters")
+
+    for ch_idx in range(weights_l.shape[-1]):
+        fig.add_subplot(fig_dim, fig_dim, ch_idx + 1)
+        plt.imshow(weights_l[:, :, ch_idx], cmap='Greys', vmin=min_filt_value, vmax=max_filt_value)
--- utils.py
+++ utils.py
@@ -3,6 +3,10 @@
 # -------------------------------------------------------------------------------------------
 import keras
 from keras.datasets import mnist
+import keras.backend as K
+import matplotlib.pyplot as plt
+
+import numpy as np
 
 MNIST_NUM_CLASSES = 10
 
@@ -32,3 +36,44 @@ def get_mnist_data(sample_idx=0):
     label_sample = label_test[sample_idx]
 
     return data_train, label_train, data_test, label_test, data_sample, label_sample
+
+
+def display_layer_activations(model, layer_idx, data_sample):
+    """
+    Display the activation volume of the specified layer. Each feature map is displayed in a separate subplot.
+    Expected format of layers is [b, r, c, ch]
+
+    :param model:
+    :param layer_idx:
+    :param data_sample:
+    :return:
+    """
+
+    # Define a function to get the activation volume
+    get_layer_output = K.function(
+        [model.layers[0].input, K.learning_phase()],
+        [model.layers[layer_idx].output]
+    )
+
+    # Get the activations in a usable format
+    act_volume = np.asarray(get_layer_output(
+        [data_sample, 0],  # second input specifies the learning phase 0=output, 1=training
+    ))
+
+    # Reshape the activations, the casting above adds another dimension
+    act_volume = act_volume.reshape(
+        act_volume.shape[1],
+        act_volume.shape[2],
+        act_volume.shape[3],
+        act_volume.shape[4]
+    )
+
+    max_ch = np.int(np.round(np.sqrt(act_volume.shape[-1])))
+
+    f = plt.figure()
+
+    for ch_idx in range(act_volume.shape[-1]):
+        f.add_subplot(max_ch, max_ch, ch_idx + 1)
+        plt.imshow(act_volume[0, :, :, ch_idx], cmap='Greys')
+
+    f.suptitle("Feature maps of layer @ idx %d: %s" % (layer_idx, model.layers[layer_idx].name))
