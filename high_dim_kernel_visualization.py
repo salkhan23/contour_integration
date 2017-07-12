ee94066e5bede5a2df874c7cdea1402a4ac86ac0
Salman <salkhan23@gmail.com>
2017-07-11 20:40:04 -0400

    Make the high dim filter visualization technique into a callable function

--- high_dim_kernel_visualization.py
+++ high_dim_kernel_visualization.py
@@ -24,13 +24,13 @@ from keras import backend as K
 import utils
 reload(utils)
 
-# Dimensions of the generated pictures for each filter
-IMG_WIDTH = 3
-IMG_HEIGHT = 3
-NUM_ITERATIONS = 20  # Iterations of the gradient ascent
-
-# The name of the layer we want to visualize
-LAYER_NAME = 'block1_conv1'
+# # Dimensions of the generated pictures for each filter
+# IMG_WIDTH = 3
+# IMG_HEIGHT = 3
+# NUM_ITERATIONS = 20  # Iterations of the gradient ascent
+#
+# # The name of the layer we want to visualize
+# LAYER_NAME = 'block1_conv1'
 
 
 def normalize(x):
@@ -43,35 +43,33 @@ def normalize(x):
     return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
 
 
-if __name__ == "__main__":
+def display_hd_filter_opt_stimuli(model, layer_idx, gen_img_row=7, gen_img_col=7, num_iterations=20, margin=1):
+    """
 
-    plt.ion()
+    :param margin:
+    :param num_iterations:
+    :param gen_img_col:
+    :param gen_img_row:
+    :param layer_idx:
+    :param model:
+    :return:
+    """
+    w_mat = model.layers[layer_idx].weights
+    if not w_mat:
+        raise Exception("No weights in layer at index %d" % layer_idx)
 
-    model = vgg16.VGG16(weights='imagenet', include_top=False)
-    model.summary()
+    # TODO: Generalize to Theano format
+    r, c, in_ch, out_ch = K.int_shape(w_mat)
 
-    layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
-
-    weights = layer_dict[LAYER_NAME].weights[0]  # Only the weights not the bias
-    r, c, in_ch, out_ch = K.int_shape(weights)
-
-    # 1. Display the filters of the first layer
-    # --------------------------------------------------------------------
-    utils.display_filters(weights)
-    plt.title("Actual Kernels of layer %s" %LAYER_NAME)
-
-    # 2. Display activations that maximally activate a neuron
-    # This is the main tutorial
-    # --------------------------------------------------------------------
     input_img = model.input
-
     keep_filters = []
+
     for filter_idx in range(0, out_ch):
         print("Processing filter %d" % filter_idx)
         start_time = time.time()
 
         # Build a loss function that maximizes the activation of the nth filter of the layer being considered
-        layer_output = layer_dict[LAYER_NAME].output
+        layer_output = model.layers[layer_idx].output
 
         if K.image_data_format() == 'channel_first':
             loss = K.mean(layer_output[:, filter_idx, :, :])
@@ -90,13 +88,13 @@ if __name__ == "__main__":
 
         # we start from a gray image with some random noise
         if K.image_data_format() == 'channel_first':
-            input_img_data = np.random.random((1, in_ch, IMG_WIDTH, IMG_HEIGHT))  # generates random floats b\w (0,1)
+            input_img_data = np.random.random((1, in_ch, gen_img_row, gen_img_col))  # generates random floats b\w (0,1)
         else:
-            input_img_data = np.random.random((1, IMG_WIDTH, IMG_HEIGHT, in_ch))  # generates random floats b\w (0,1)
-        input_img_data = (input_img_data - 0.5) * NUM_ITERATIONS + 128  # Why x20? ## number of steps?
+            input_img_data = np.random.random((1, gen_img_row, gen_img_col, in_ch))  # generates random floats b\w (0,1)
+        input_img_data = (input_img_data - 0.5) * num_iterations + 128
 
-        # Run gradient ascent for 20 steps
-        for i in range(NUM_ITERATIONS):
+        # Run gradient ascent for num_iterations steps
+        for i in range(num_iterations):
             loss_value, grads_value = iterate([input_img_data])
             input_img_data += grads_value
 
@@ -105,35 +103,144 @@ if __name__ == "__main__":
                 break  # some filters get stuck to 0, we can skip them
 
         # Decode the resulting input image
-        # if loss_value > 0:
         img = utils.deprocess_image(input_img_data[0])  # removes the batch index
         keep_filters.append((img, loss_value))
 
         end_time = time.time()
-        print('Filter %d processes in %ds' % (filter_idx, end_time - start_time))
+        print('Filter %d processed in %ds' % (filter_idx, end_time - start_time))
 
-    # Stitch the best 64 filters on a 8x8 grid
-    n = 8
-    #
-    # # filters with the highest loss are assumed to be better looking
-    # keep_filters.sort(key=lambda x: x[1], reverse=True)
-    # keep_filters = keep_filters[:n * n]
-    #
-    # build a black picture with enough space for our 8x8 filters of size 128x128 with a 5pnt margin between
-    margin = 1
-    width = n * IMG_WIDTH + (n - 1) * margin
-    height = n * IMG_HEIGHT + (n - 1) * margin
-    stitched_filters = np.zeros((width, height, 3))
+    # Display the generated images
+    n = np.int(np.round(np.sqrt(out_ch)))  # Single dimension of tiled image
 
-    # Fill in the picture with the saved filters
-    for i in range(n):
-        for j in range(n):
-            img, loss = keep_filters[i * n + j]
-            stitched_filters[(IMG_WIDTH + margin) * i: (IMG_WIDTH + margin)*i + IMG_WIDTH,
-                             (IMG_HEIGHT + margin) * j:(IMG_HEIGHT+margin)*j + IMG_HEIGHT, :] = img
+    width = (n * r) + ((n - 1) * margin)
+    height = (n * c) + ((n - 1) * margin)
 
-    # save the results
+    tiled_filters = np.zeros((width, height, in_ch))
+
+    # Fill in in composite image with the filters
+    for r_idx in range(n):
+        for c_idx in range(n):
+
+            filt_idx = (r_idx * n) + c_idx
+            if filt_idx >= out_ch:
+                break
+
+            print("Processing filter %d" % filt_idx)
+
+            tiled_filters[
+                (gen_img_row + margin) * r_idx: (gen_img_row + margin) * r_idx + gen_img_row,
+                (gen_img_col + margin) * c_idx: (gen_img_col + margin) * c_idx + gen_img_col,
+                :
+            ] = utils.deprocess_image(K.eval(weights[:, :, :, filt_idx]))
+
+    # Plot the Composite Figure
+    plt.ion()
     plt.figure()
-    plt.imshow(stitched_filters)
-    plt.title("Max activations of individual filters at Layer %s" % LAYER_NAME)
+    plt.imshow(tiled_filters)
     plt.colorbar()
+
+
+if __name__ == "__main__":
+
+    plt.ion()
+
+    vgg16_model = vgg16.VGG16(weights='imagenet', include_top=False)
+    vgg16_model.summary()
+
+
+
+
+
+
+
+
+
+    #
+    # layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
+    #
+    # weights = layer_dict[LAYER_NAME].weights[0]  # Only the weights not the bias
+    # # r, c, in_ch, out_ch = K.int_shape(weights)
+    #
+    # # 1. Display the filters of the first layer
+    # # --------------------------------------------------------------------
+    # utils.display_filters(weights)
+    # plt.title("Actual Kernels of layer %s" %LAYER_NAME)
+    #
+    # # 2. Display activations that maximally activate a neuron
+    # # This is the main tutorial
+    # # --------------------------------------------------------------------
+
+    # input_img = model.input
+    #
+    # keep_filters = []
+    # for filter_idx in range(0, out_ch):
+    #     print("Processing filter %d" % filter_idx)
+    #     start_time = time.time()
+    #
+    #     # Build a loss function that maximizes the activation of the nth filter of the layer being considered
+    #     layer_output = layer_dict[LAYER_NAME].output
+    #
+    #     if K.image_data_format() == 'channel_first':
+    #         loss = K.mean(layer_output[:, filter_idx, :, :])
+    #     else:
+    #         loss = K.mean(layer_output[:, :, :, filter_idx])
+    #
+    #     # Compute the gradient of the input picture wrt this loss
+    #     grads = K.gradients(loss, input_img)[0]  # Get the gradient of the loss function wrt input image
+    #     grads = normalize(grads)  # avoids very large or very small gradients and ensures a smooth ascent
+    #
+    #     # This function returns the loss and the grads given the input picture
+    #     iterate = K.function([input_img], [loss, grads])
+    #
+    #     # Step size for gradient ascent
+    #     step = 1.
+    #
+    #     # we start from a gray image with some random noise
+    #     if K.image_data_format() == 'channel_first':
+    #         input_img_data = np.random.random((1, in_ch, IMG_WIDTH, IMG_HEIGHT))  # generates random floats b\w (0,1)
+    #     else:
+    #         input_img_data = np.random.random((1, IMG_WIDTH, IMG_HEIGHT, in_ch))  # generates random floats b\w (0,1)
+    #     input_img_data = (input_img_data - 0.5) * NUM_ITERATIONS + 128  # Why x20? ## number of steps?
+    #
+    #     # Run gradient ascent for 20 steps
+    #     for i in range(NUM_ITERATIONS):
+    #         loss_value, grads_value = iterate([input_img_data])
+    #         input_img_data += grads_value
+    #
+    #         print("Current Loss value:", loss_value)
+    #         if loss_value <= 0.0:
+    #             break  # some filters get stuck to 0, we can skip them
+    #
+    #     # Decode the resulting input image
+    #     # if loss_value > 0:
+    #     img = utils.deprocess_image(input_img_data[0])  # removes the batch index
+    #     keep_filters.append((img, loss_value))
+    #
+    #     end_time = time.time()
+    #     print('Filter %d processes in %ds' % (filter_idx, end_time - start_time))
+    #
+    # # Stitch the best 64 filters on a 8x8 grid
+    # n = 8
+    # #
+    # # # filters with the highest loss are assumed to be better looking
+    # # keep_filters.sort(key=lambda x: x[1], reverse=True)
+    # # keep_filters = keep_filters[:n * n]
+    # #
+    # # build a black picture with enough space for our 8x8 filters of size 128x128 with a 5pnt margin between
+    # margin = 1
+    # width = n * IMG_WIDTH + (n - 1) * margin
+    # height = n * IMG_HEIGHT + (n - 1) * margin
+    # stitched_filters = np.zeros((width, height, 3))
+    #
+    # # Fill in the picture with the saved filters
+    # for i in range(n):
+    #     for j in range(n):
+    #         img, loss = keep_filters[i * n + j]
+    #         stitched_filters[(IMG_WIDTH + margin) * i: (IMG_WIDTH + margin)*i + IMG_WIDTH,
+    #                          (IMG_HEIGHT + margin) * j:(IMG_HEIGHT+margin)*j + IMG_HEIGHT, :] = img
+    #
+    # # save the results
+    # plt.figure()
+    # plt.imshow(stitched_filters)
+    # plt.title("Max activations of individual filters at Layer %s" % LAYER_NAME)
+    # plt.colorbar()
