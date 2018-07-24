1b2d3a8b6e2753b2d5ff7e0b117d403be8177c3e
Salman <salkhan23@gmail.com>
2018-07-24 19:35:53 -0400

    Update and clean up generate curved contour_data set. Some more debuggin points added

--- generate_curved_contour_data_set.py
+++ generate_curved_contour_data_set.py
@@ -13,14 +13,17 @@ import datetime
 import keras.backend as keras_backend
 
 import image_generator_curve
-from base_models import alex_net
+
 import gabor_fits
 import generate_curved_contour_data_set_orient_matched as generate_contour_images
+import contour_integration_models.alex_net.model_3d as contour_integration_model_3d
+import alex_net_utils
 
 reload(image_generator_curve)
-reload(alex_net)
 reload(gabor_fits)
 reload(generate_contour_images)
+reload(contour_integration_model_3d)
+reload(alex_net_utils)
 
 DATA_DIRECTORY = "./data/curved_contours/filt_matched_frag"
 
@@ -47,20 +50,31 @@ if __name__ == '__main__':
     if not os.path.exists(DATA_DIRECTORY):
         os.makedirs(DATA_DIRECTORY)
 
-    # contour_len_arr = np.array([1, 3, 5, 7, 9])
-    # beta_rotation_arr = np.array([0, 15, 30, 45, 60])
+    # -----------------------------------------------------------------------------------
+    # Contour Integration Model
+    # -----------------------------------------------------------------------------------
+    print("Building Contour Integration Model...")
 
-    # -----------------------------------------------------------------------------------
-    # Target Kernel
-    # -----------------------------------------------------------------------------------
-    tgt_filter = alex_net.get_target_feature_extracting_kernel(tgt_filter_idx)
+    cont_int_model = contour_integration_model_3d.build_contour_integration_model(
+        tgt_filt_idx=0,
+        rf_size=25,
+        inner_leaky_relu_alpha=0.7,
+        outer_leaky_relu_alpha=0.94,
+        l1_reg_loss_weight=0.01
+    )
+    # cont_int_model.summary()
+
+    feat_extract_callback = alex_net_utils.get_activation_cb(cont_int_model, 1)
+
+    feat_extract_kernels, _ = cont_int_model.layers[1].get_weights()
+    tgt_filter = feat_extract_kernels[:, :, :, tgt_filter_idx]
     print("Feature extracting kernel @ index {} selected.".format(tgt_filter_idx))
 
     # -----------------------------------------------------------------------------------
     # Contour Fragment
     # -----------------------------------------------------------------------------------
     best_fit_params_list = gabor_fits.find_best_fit_2d_gabor(tgt_filter)
-    #
+
     # Create list of dictionaries of gabor params
     gabor_params_dict_list = []
     for chan_params in best_fit_params_list:
@@ -82,6 +96,12 @@ if __name__ == '__main__':
     # Plot rotations of the fragment
     gabor_fits.plot_fragment_rotations(fragment, gabor_params_dict_list[0], delta_rot=15)
 
+    # Get most responsive kernel and activation value
+    max_active_kernel, max_act_value = alex_net_utils.find_most_active_l1_kernel_index(
+        fragment, feat_extract_callback)
+    print("Most active Kernel @ {}, Activation Value {}".format(
+        max_active_kernel, max_act_value))
+
     # ------------------------------------------------------------------------------------
     # Generate Images
     # ------------------------------------------------------------------------------------
@@ -93,7 +113,7 @@ if __name__ == '__main__':
         frag=fragment,
         frag_params=gabor_params_dict_list,
         f_tile_size=full_tile_size,
-        img_size=(227, 227, 3)
+        img_size=image_size
     )
 
     print("Generating Test Data Set")
@@ -104,28 +124,7 @@ if __name__ == '__main__':
         frag=fragment,
         frag_params=gabor_params_dict_list,
         f_tile_size=full_tile_size,
-        img_size=(227, 227, 3)
+        img_size=image_size
     )
 
     print("Total Time {}".format(datetime.datetime.now() - start_time))
-
-    # # -----------------------------------------------------------------------------------
-    # #  Debug
-    # # -----------------------------------------------------------------------------------
-    # # Load a sample image to see that it is created and stored correctly
-    # contour_len = 9
-    # inter_fragment_rotation = 15
-    # image_idx = 0
-    # test_img_loc = os.path.join(
-    #     DATA_DIRECTORY,
-    #     'test',
-    #     'filter_{}'.format(tgt_filter_idx),
-    #     "c_len_{0}".format(contour_len),
-    #     "beta_{0}".format(inter_fragment_rotation),
-    #     "c_len_{0}_beta_{1}__{2}.png".format(contour_len, inter_fragment_rotation, image_idx)
-    # )
-    #
-    # img = load_img(test_img_loc)
-    # plt.figure()
-    # plt.imshow(img)
-    # plt.title("Sample Generated Image")
