11d8cfd520dcc627918e54f292398d235f6bc6da
Salman <salkhan23@gmail.com>
2018-08-29 20:58:12 -0400

    Add a foutine to measure fragment space vs gain performance. Also fix get training data dictionaries for the two existing routines

--- field_1993_routines.py
+++ field_1993_routines.py
@@ -68,7 +68,7 @@ def contour_gain_vs_inter_fragment_rotation(
     # --------------------------------------
     # Get Neurophysiological Data
     # --------------------------------------
-    abs_gains_arr = generate_curved_contour_data_set_orient_matched.get_neurophysiological_data()
+    abs_gains_arr = generate_curved_contour_data_set_orient_matched.get_neurophysiological_data('c_len')
     inter_frag_rotation_arr = np.array([0, 15, 30, 45, 60])
 
     # Plot Neurophysiological data
@@ -184,7 +184,7 @@ def contour_gain_vs_length(model, data_key, beta, frag_orient=None, alpha=0, n_r
 
     # --------------------------------------
     # Get Neurophysiological Data
-    abs_gains_arr = generate_curved_contour_data_set_orient_matched.get_neurophysiological_data()
+    abs_gains_arr = generate_curved_contour_data_set_orient_matched.get_neurophysiological_data('c_len')
     c_len_arr = np.array([1, 3, 5, 7, 9])
 
     expected_gains = [abs_gains_arr[c_len][alpha][beta] for c_len in c_len_arr]
@@ -246,6 +246,121 @@ def contour_gain_vs_length(model, data_key, beta, frag_orient=None, alpha=0, n_r
     axis.set_xlabel("Contour Length")
     axis.set_ylabel("Gain")
     axis.set_title("Enhancement gain vs Contour Length")
+
+
+def contour_gain_vs_spacing(model, data_key, beta, frag_orient=None, alpha=0, n_runs=100, axis=None):
+    """
+
+    todo: Fix the description!
+    Model contour enhancement gain as a function of fragment spacing. This is similar
+    to an experiment from Li-2006 except that additionally contour curvature is considered.
+
+    This is a derived Neurophysiological Result. Expected Gain for a given length is found by
+    multiplying the relative curvature gain from Fields 1993 with the absolute gain for a linear
+    contour as specified by Li -2006. Not that inter-fragment spacing is different from
+    Li 2006 Results.
+
+    Detectability = 100%, Absolute enhancement gain = Full linear gain
+    Detectability = 50%, absolute enhancement gain = 1
+
+    TODO: Inter-fragment spacing is currently not accounted for. Li-2006 presents
+    TODO: enhancement gain for relative spacing between fragments. The considered spacing
+    TODO: are too small for the base spacing considered in Fields-1993.
+    TODO: Need to properly account for these.
+
+    Different from the previous version of this routine (Li2006Routines), here stimuli are
+    generated based on Fields -1993 Method.
+
+    :param model: Contour Integration Model
+        (Should be training model with last layer = enhancement gain calculating layer)
+    :param data_key: data key (dictionary of dictionaries) that describes the data
+    :param beta: Consider contours with inter-fragment rotations of this amount
+    :param alpha:
+    :param frag_orient: Default orientation of contour fragment. [-90 = horizontal, 0 = vertical]
+    :param n_runs: number of runs to average results over for each point
+    :param axis: [Default None]
+
+    :return:
+    """
+    # --------------------------------------
+    # Validation
+    # --------------------------------------
+    valid_beta = [0, 15, 30, 45, 60]
+    if beta not in valid_beta:
+        raise Exception("Invalid inter-fragment rotation {0}. Allowed {1}".format(beta, valid_beta))
+
+    valid_alpha = [0, 15, 30]
+    if alpha not in valid_alpha:
+        raise Exception("Invalid alpha {0} specified. Allowed {1}".format(alpha, valid_alpha))
+
+    print("Model Contour Gain vs fragment spacing "
+          "for inter-fragment rotation {0}, frag orientation {1}, alpha={2}".format(beta, frag_orient, alpha))
+
+    # --------------------------------------
+    # Get Neurophysiological Data
+    abs_gains_arr = generate_curved_contour_data_set_orient_matched.get_neurophysiological_data('f_spacing')
+
+    f_spacing_arr = np.array([1, 1.2, 1.4, 1.6, 1.9])
+
+    expected_gains = [abs_gains_arr[f_spacing][alpha][beta] for f_spacing in f_spacing_arr]
+
+    # Plot Neurophysiological data
+    # --------------------------------------
+    axis.plot(f_spacing_arr, expected_gains,
+              label='Fields1993+Li2006-beta{}'.format(beta), marker='s', linestyle='--')
+
+    # --------------------------------------
+    # Model Results
+    # --------------------------------------
+    avg_gain_per_f_spacing = []
+    std_gain_per_f_spacing = []
+
+    for f_spacing in f_spacing_arr:
+
+        print("Processing fragment spacing = {}, beta = {}".format(f_spacing, beta))
+
+        # filter the data keys
+        use_set = data_key
+        use_set = [x for x in use_set if 'f_spacingx10_{}'.format(int(f_spacing * 10)) in x]
+        use_set = [x for x in use_set if 'beta_{}'.format(beta) in x]
+        use_set = [x for x in use_set if 'alpha_{}'.format(alpha) in x]
+        if frag_orient is not None:
+            use_set = [x for x in use_set if 'forient_{}'.format(frag_orient) in x]
+
+        active_test_set = {}
+        for set_id in use_set:
+            active_test_set.update(data_key[set_id])
+
+        image_generator = image_generator_curve.DataGenerator(
+            active_test_set,
+            batch_size=1,
+            shuffle=True,
+        )
+
+        gen_out = iter(image_generator)
+
+        # Get the results
+        y_hat_arr = []
+        for r_idx in range(n_runs):
+            x_in, y = gen_out.next()
+
+            # TODO: look into using activations callbacks. Then this routine can be used by
+            # TODO: the full contour integration model, which does not have a gain calculating layer.
+            y_hat = model.predict(x_in, batch_size=1)
+            y_hat_arr.append(y_hat)
+            # print("Predicted gain {0}, Expected gain {1}".format(y_hat, y))
+
+        avg_gain_per_f_spacing.append(np.mean(y_hat_arr))
+        std_gain_per_f_spacing.append(np.std(y_hat_arr))
+
+    axis.errorbar(
+        f_spacing_arr, avg_gain_per_f_spacing, std_gain_per_f_spacing,
+        marker='o', label='Model-beta_{}'.format(beta), linestyle='-')
+
+    axis.legend()
+    axis.set_xlabel("Contour Length")
+    axis.set_ylabel("Gain")
+    axis.set_title("Enhancement gain vs Fragment Spacing")
 
 
 def plot_activations(model, img_file, tgt_filt_idx,):
