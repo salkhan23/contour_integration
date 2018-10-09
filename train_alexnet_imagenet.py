e8ed1a60111259c4de14c024968a4efd8978dd00
Salman <salkhan23@gmail.com>
2018-10-09 10:21:46 -0400

    Enable the rest of the alexnet training script

--- train_alexnet_imagenet.py
+++ train_alexnet_imagenet.py
@@ -86,123 +86,123 @@ if __name__ == '__main__':
 
     model.summary()
 
-    # # -----------------------------------------------------------------------------------
-    # # Training
-    # # -----------------------------------------------------------------------------------
-    # print("Building Data Generators ...")
+    # -----------------------------------------------------------------------------------
+    # Training
+    # -----------------------------------------------------------------------------------
+    print("Building Data Generators ...")
+
+    train_datagen = ImageDataGenerator(
+        data_format='channels_first',
+        preprocessing_function=preprocessing_function
+    )
+    test_datagen = ImageDataGenerator(
+        data_format='channels_first',
+        preprocessing_function=preprocessing_function
+    )
+
+    # train_datagen = CenteredImageDataGenerator(
+    #     data_format='channels_first', featurewise_center=True)
+    # test_datagen = CenteredImageDataGenerator(data_format='channels_first', featurewise_center=True)
+
+    train_generator = train_datagen.flow_from_directory(
+        IMAGE_NET_TRAIN_DIR,
+        target_size=(227, 227),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=True,
+        seed=42
+    )
+
+    validation_generator = test_datagen.flow_from_directory(
+        IMAGE_NET_VALIDATION_DIR,
+        target_size=(227, 227),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=True,
+        seed=42
+    )
+
+    # Callback
+    TEMP_WEIGHT_STORE_FILE = os.path.join(RESULTS_DIR, 'trained_alexnet_weights_lowest_cost.hf')
+    checkpoint = ModelCheckpoint(
+        TEMP_WEIGHT_STORE_FILE,
+        monitor='val_loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min',
+        save_weights_only=True,
+    )
+
+    # -----------------------------------------------------------------------------------
+    print("Start Training ...")
+    start_time = datetime.now()
+
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=(n_train_images // batch_size),
+        epochs=n_epochs,
+        validation_data=validation_generator,
+        validation_steps=50,  # todo : Fixme    n_test_images // batch_size
+        verbose=1,
+        use_multiprocessing=False,
+        callbacks=[checkpoint]
+    )
+
+    model.save_weights(os.path.join(RESULTS_DIR, "trained_alexnet_weights_final_cost.hf"))
+    print("Training took {}".format(datetime.now() - start_time))
+
+    # Plot Loss
+    f, axis = plt.subplots()
+
+    axis.plot(history.history['loss'], label='train_loss')
+    axis.plot(history.history['val_loss'], label='validation_loss')
+    axis.set_xlabel("Epoch")
+    axis.set_ylabel("Loss")
+    axis.legend()
+
+    # -----------------------------------------------------------------------------------
+    # Test Predictions of the Model
+    # -----------------------------------------------------------------------------------
+    print("Testing Final performance")
+    test_generator = test_datagen.flow_from_directory(
+        directory=IMAGE_NET_VALIDATION_DIR,
+        target_size=(227, 227),
+        batch_size=1,
+        class_mode='categorical',
+        shuffle=False,
+    )
+
+    test_generator.reset()
+    pred = model.predict_generator(test_generator, verbose=1)
+
+    predicted_class_indices = np.argmax(pred, axis=1)
+
+    labels = train_generator.class_indices
+    labels = dict((v, k) for k, v in labels.items())
+    predictions = [labels[k] for k in predicted_class_indices]
+    # # Change labels to words
+    # readable_predictions = imagenet_utils.decode_predictions(pred)
+
     #
-    # train_datagen = ImageDataGenerator(
-    #     data_format='channels_first',
-    #     preprocessing_function=preprocessing_function
-    # )
-    # test_datagen = ImageDataGenerator(
-    #     data_format='channels_first',
-    #     preprocessing_function=preprocessing_function
-    # )
-    #
-    # # train_datagen = CenteredImageDataGenerator(
-    # #     data_format='channels_first', featurewise_center=True)
-    # # test_datagen = CenteredImageDataGenerator(data_format='channels_first', featurewise_center=True)
-    #
-    # train_generator = train_datagen.flow_from_directory(
-    #     IMAGE_NET_TRAIN_DIR,
-    #     target_size=(227, 227),
-    #     batch_size=batch_size,
-    #     class_mode='categorical',
-    #     shuffle=True,
-    #     seed=42
-    # )
-    #
-    # validation_generator = test_datagen.flow_from_directory(
-    #     IMAGE_NET_VALIDATION_DIR,
-    #     target_size=(227, 227),
-    #     batch_size=batch_size,
-    #     class_mode='categorical',
-    #     shuffle=True,
-    #     seed=42
-    # )
-    #
-    # # Callback
-    # TEMP_WEIGHT_STORE_FILE = os.path.join(RESULTS_DIR, 'trained_alexnet_weights_lowest_cost.hf')
-    # checkpoint = ModelCheckpoint(
-    #     TEMP_WEIGHT_STORE_FILE,
-    #     monitor='val_loss',
-    #     verbose=0,
-    #     save_best_only=True,
-    #     mode='min',
-    #     save_weights_only=True,
-    # )
-    #
-    # # -----------------------------------------------------------------------------------
-    # print("Start Training ...")
-    # start_time = datetime.now()
-    #
-    # history = model.fit_generator(
-    #     train_generator,
-    #     steps_per_epoch=(n_train_images // batch_size),
-    #     epochs=n_epochs,
-    #     validation_data=validation_generator,
-    #     validation_steps=50,  # todo : Fixme    n_test_images // batch_size
-    #     verbose=1,
-    #     use_multiprocessing=False,
-    #     callbacks=[checkpoint]
-    # )
-    #
-    # model.save_weights(os.path.join(RESULTS_DIR, "trained_alexnet_weights_final_cost.hf"))
-    # print("Training took {}".format(datetime.now() - start_time))
-    #
-    # # Plot Loss
-    # f, axis = plt.subplots()
-    #
-    # axis.plot(history.history['loss'], label='train_loss')
-    # axis.plot(history.history['val_loss'], label='validation_loss')
-    # axis.set_xlabel("Epoch")
-    # axis.set_ylabel("Loss")
-    # axis.legend()
-    #
-    # # -----------------------------------------------------------------------------------
-    # # Test Predictions of the Model
-    # # -----------------------------------------------------------------------------------
-    # print("Testing Final performance")
-    # test_generator = test_datagen.flow_from_directory(
-    #     directory=IMAGE_NET_VALIDATION_DIR,
-    #     target_size=(227, 227),
-    #     batch_size=1,
-    #     class_mode='categorical',
-    #     shuffle=False,
-    # )
-    #
-    # test_generator.reset()
-    # pred = model.predict_generator(test_generator, verbose=1)
-    #
-    # predicted_class_indices = np.argmax(pred, axis=1)
-    #
-    # labels = train_generator.class_indices
-    # labels = dict((v, k) for k, v in labels.items())
-    # predictions = [labels[k] for k in predicted_class_indices]
-    # # # Change labels to words
-    # # readable_predictions = imagenet_utils.decode_predictions(pred)
-    #
-    # #
-    # filenames = test_generator.filenames
-    # y_true = []
-    # is_same = np.zeros(len(filenames))
-    #
-    # for f_idx, filename in enumerate(filenames):
-    #     label = os.path.dirname(filename)
-    #     if '/' in label:
-    #         label = label.strip('/')[1]
-    #     y_true.append(label)
-    #     is_same[f_idx] = (y_true[f_idx] == predictions[f_idx])
-    #
-    # # Write Data to File
-    # results = pd.DataFrame(
-    #     {"Filename": filenames,
-    #      "y_true": y_true,
-    #      "Predictions": predictions,
-    #      "Are Equal": is_same
-    #      }
-    # )
-    # results.to_csv(os.path.join(RESULTS_DIR, "results.csv"), index=False)
-    #
-    # print("Final prediction Accuracy {}".format(np.mean(is_same)))
+    filenames = test_generator.filenames
+    y_true = []
+    is_same = np.zeros(len(filenames))
+
+    for f_idx, filename in enumerate(filenames):
+        label = os.path.dirname(filename)
+        if '/' in label:
+            label = label.strip('/')[1]
+        y_true.append(label)
+        is_same[f_idx] = (y_true[f_idx] == predictions[f_idx])
+
+    # Write Data to File
+    results = pd.DataFrame(
+        {"Filename": filenames,
+         "y_true": y_true,
+         "Predictions": predictions,
+         "Are Equal": is_same
+         }
+    )
+    results.to_csv(os.path.join(RESULTS_DIR, "results.csv"), index=False)
+
+    print("Final prediction Accuracy {}".format(np.mean(is_same)))
