31bed4c8b3f513f5e11558ffb68b36a245b7e3fc
Salman <salkhan23@gmail.com>
2019-03-07 09:36:42 -0500

    plot input to the contour inegration layeras well. Some cleanup

--- visualize_multi_kernel_trained_model.py
+++ visualize_multi_kernel_trained_model.py
@@ -50,18 +50,26 @@ def plot_max_contour_enhancement(img, input_cb, cont_int_cb):
     max_gain = max_gain[0, ]  # get rid of batch dim
 
     # Plots
-    f = plt.figure(figsize=(15, 5))
+    f = plt.figure(figsize=(20, 5))
 
-    f.add_subplot(1, 3, 1)
+    f.add_subplot(1, 4, 1)
     plt.imshow(img, cmap='Greys')
     plt.colorbar(orientation='horizontal')
 
-    f.add_subplot(1, 3, 2)
+    f.add_subplot(1, 4, 2)
+    input_act_max = np.max(input_act, axis=1)
+    input_act_max = input_act_max[0, ]
+    max_max_input_act = np.max(input_act_max)
+    plt.imshow(input_act_max, cmap='seismic', vmax=max_max_input_act, vmin=-max_max_input_act)
+    plt.colorbar(orientation='horizontal')
+    plt.title("Feature Extract activation")
+
+    f.add_subplot(1, 4, 3)
     plt.imshow(max_diff_across_chan, cmap='seismic', vmax=max_max_diff, vmin=-max_max_diff)
     plt.colorbar(orientation='horizontal')
     plt.title("Maximum Difference @ each (x,y)")
 
-    f.add_subplot(1, 3, 3)
+    f.add_subplot(1, 4, 4)
     plt.imshow(max_gain, cmap='seismic', vmax=-0.5, vmin=2.5)
     plt.colorbar(orientation='horizontal')
     plt.title("Max Gain @ each (x,y)")
@@ -206,14 +214,23 @@ def main(model, g_params, learnt_kernels):
     #     test_image, cont_int_input_act_cb, cont_int_act_cb, learnt_kernels)
 
     # ----------------------------------------------------------------------------------
-    # 4. Natural Image
+    # 4. Natural Images
     # ----------------------------------------------------------------------------------
-    image_file = './data/sample_images/cat.7.jpg'
-    test_image = load_and_preprocess_image(image_file)
+    list_of_images = [
+        './data/sample_images/cat.23.jpg',
+        './data/sample_images/cat.7.jpg',
+        './data/sample_images/dog.12447.jpg',
+        './data/sample_images/zahra.jpg',
+        # './data/sample_images/square.png',
+    ]
 
-    plot_max_contour_enhancement(test_image, cont_int_input_act_cb, cont_int_act_cb)
-    # plot_contour_enhancement_individual_kernels(
-    #     test_image, cont_int_input_act_cb, cont_int_act_cb, learnt_kernels)
+    for image_file in list_of_images:
+
+        test_image = load_and_preprocess_image(image_file)
+
+        plot_max_contour_enhancement(test_image, cont_int_input_act_cb, cont_int_act_cb)
+        # plot_contour_enhancement_individual_kernels(
+        #     test_image, cont_int_input_act_cb, cont_int_act_cb, learnt_kernels)
 
 
 if __name__ == '__main__':
@@ -230,29 +247,29 @@ if __name__ == '__main__':
     # -----------------------------------------------------------------------------------
     # Base Gabor
     # -----------------------------------------------------------------------------------
-    # # Gabor Params for Filter @ index 5
-    # gabor_params = {
-    #     'x0': 0,
-    #     'y0': 0,
-    #     'theta_deg': -90.0,
-    #     'amp': 1,
-    #     'sigma': 2.75,
-    #     'lambda1': 15.0,
-    #     'psi': 1.5,
-    #     'gamma': 0.8
-    # }
-
-    # Gabor Params for Filter @ index 10
+    # Gabor Params for Filter @ index 5
     gabor_params = {
         'x0': 0,
         'y0': 0,
-        'theta_deg': 0.0,
+        'theta_deg': -90.0,
         'amp': 1,
         'sigma': 2.75,
-        'lambda1': 5.5,
-        'psi': 1.25,
+        'lambda1': 15.0,
+        'psi': 1.5,
         'gamma': 0.8
     }
+
+    # # Gabor Params for Filter @ index 10
+    # gabor_params = {
+    #     'x0': 0,
+    #     'y0': 0,
+    #     'theta_deg': 0.0,
+    #     'amp': 1,
+    #     'sigma': 2.75,
+    #     'lambda1': 5.5,
+    #     'psi': 1.25,
+    #     'gamma': 0.8
+    # }
 
     # -----------------------------------------------------------------------------------
     # Model
@@ -276,7 +293,7 @@ if __name__ == '__main__':
     # contour_int_weights = \
     #     "./results/optimal_gabors_with_rotations_5_10_orientation/contour_integration_layer_weights.hf"
     contour_int_weights = \
-        "./results/optimal_gabors_with_rotations_5_10_threshold/contour_integration_layer_weights.hf"
+        "./results/all_kernels_alpha_0_beta_upto30/contour_integration_layer_weights.hf"
     cont_int_model = model_3d_all_kernels.training_model(
         rf_size=35,
         inner_leaky_relu_alpha=0.9,
@@ -285,20 +302,22 @@ if __name__ == '__main__':
     )
     cont_int_model.load_weights(contour_int_weights, by_name=True)
 
-    # trained_kernels = [5, 10, 19, 20, 21, 22, 48, 49, 51, 59, 60, 62, 64, 65, 66, 68, 69, 72, 73, 74, 76, 77, 79]
-    trained_kernels = [5, 10]
+    trained_kernels = [5, 10, 19, 20, 21, 22, 48, 49, 51, 59, 60, 62, 64, 65, 66, 68, 69, 72, 73, 74, 76, 77, 79]
+    # trained_kernels = [5, 10]
+
+    # alex_net_utils.clear_unlearnt_contour_integration_kernels(cont_int_model, trained_kernels)
 
     # -----------------------------------------------------------------------------------
     # main routine
     # -----------------------------------------------------------------------------------
-
     # #  Plot All learnt kernels
     # # -------------------------
-    for kernels_idx in trained_kernels:
-        alex_net_utils.plot_start_n_learnt_contour_integration_kernels(
-            cont_int_model,
-            kernels_idx
-        )
+    # for kernels_idx in trained_kernels:
+    #     alex_net_utils.plot_start_n_learnt_contour_integration_kernels(
+    #         cont_int_model,
+    #         kernels_idx
+    #     )
+    #     raw_input('Next?')
 
     main(cont_int_model, gabor_params, trained_kernels)
 
